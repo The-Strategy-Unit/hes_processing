002bb8def02de83e22c8c9a4f6d8fd9d907d5add
tom.jemmett@gmail.com <tom.jemmett@gmail.com>
2024-06-24 21:05:09 +0000

    fixes notebook

--- apc/silver/Check Counts.py
+++ apc/silver/Check Counts.py
@@ -11,6 +11,8 @@ from pyspark.sql import DataFrame
 
 from databricks.sdk.runtime import *
 
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC
 # MAGIC ## Compare to HES Summary Statistics
@@ -20,8 +22,10 @@ from databricks.sdk.runtime import *
 # MAGIC Note, that these files suppress data by replacing counts less than 5
 # MAGIC with a *, and then rounding all other values to the nearest 5.
 
-
 # COMMAND ----------
+
+admimeth = F.col("admimeth")
+
 fce = (
     spark.read.table("hes.silver.apc")
     .filter(F.col("fce") == True)
@@ -60,8 +64,9 @@ fae_by_group = (
     .sort(["procode3", "fyear"])
 ).display()
 
+# COMMAND ----------
+
 # MAGIC %md
-# MAGIC
 # MAGIC ## Check our last episode in spell column
 # MAGIC
 # MAGIC The following produces counts by last episode in spell. It should be
@@ -69,10 +74,7 @@ fae_by_group = (
 # MAGIC the admitting episode. This is based on the end date of the discharging
 # MAGIC episode.
 
-
 # COMMAND ----------
-
-admimeth = F.col("admimeth")
 
 df = (
     spark.read.table("hes.silver.apc")
