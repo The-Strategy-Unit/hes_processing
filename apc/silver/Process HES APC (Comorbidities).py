94b8e5d8c6c0c0cca111001b019b943e87d72b34
Tom Jemmett <thomas.jemmett@nhs.net>
2024-08-02 22:36:02 +0100

    adds comorbities script

---
+++ apc/silver/Process HES APC (Comorbidities).py
@@ -0,0 +1,83 @@
+# Databricks notebook source
+import json
+import pandas as pd
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
+from functools import reduce
+
+# COMMAND ----------
+
+def open_comorbidity_file(file, scores):
+    with open(f"/Volumes/su_data/reference/raw/{file}_comorbidity_scores.json", "r") as f:
+        return (
+            spark.createDataFrame([
+                {
+                    "group": v.get("trumps", k),
+                    "score_type": s,
+                    "score": v["score"][s],
+                    "code": c
+                }
+                for k, v in json.load(f).items()
+                for c in v["codes"]
+                for s in scores
+            ])
+            .withColum("type", file)
+        )
+
+comorbidities = reduce(
+    DataFrame.unionByName,
+    [
+        open_comorbidity_file(*i)
+        for i in [
+            ("charlson", ["charlson", "quan"]),
+            ("elixhauser", ["vw", "swiss"])
+        ]
+    ]
+)
+
+# COMMAND ----------
+
+diags = spark.read.table("hes.silver.apc_diagnoses")
+
+# COMMAND ----------
+
+comorbidities_expanded = (
+    diags
+    .select(F.col("diagnosis"))
+    .distinct()
+    .join(comorbidities, F.expr("diagnosis LIKE concat(code, '%')"))
+    .persist()
+)
+
+comorbidities_expanded.display()
+
+# COMMAND ----------
+
+comorbidities_df = (
+    diags
+    .join(
+        comorbidities_expanded,
+        on=["diagnosis"]
+    )
+    .groupBy("procode3", "fyear", "epikey", "type", "group", "score_type")
+    .agg(
+        F.max("score").alias("score")
+    )
+    # handle trumps
+    .groupBy("procode3", "fyear", "epikey", "type", "score_type")
+    .agg(
+        F.sum("score").alias("score")
+    )
+    .orderBy("type", "score_type")
+)
+
+# COMMAND ----------
+
+(
+    comorbidities_df
+    .repartition("procode3")
+    .write
+    .mode("overwrite")
+    .partitionBy(["fyear", "procode3", "type", "score_type"])
+    .saveAsTable("hes.silver.apc_comorbidities")
+)
