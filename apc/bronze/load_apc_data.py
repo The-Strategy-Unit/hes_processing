ba03f896d18445a15f871a8a6bbe4fcd750844b4
Tom Jemmett <thomas.jemmett@nhs.net>
2025-02-04 20:59:42 +0000

    adds ability to delete rows from the table if we are re-running a year

--- apc/bronze/load_apc_data.py
+++ apc/bronze/load_apc_data.py
@@ -1,18 +1,31 @@
 from pyspark import SparkContext
+from pyspark.sql import functions as F
 from databricks.connect import DatabricksSession
+from delta.tables import DeltaTable
 import sys
-
-from apc.bronze.schemas import columns
 
 from apc.bronze.get_apc_csv_data import get_apc_csv_data
 from apc.bronze.get_apc_parquet_data import get_apc_parquet_data
 
+TABLE = "su_data.hes.bronze"
+
 def load_apc_data(spark: SparkContext, year: int) -> None:
-    if year >= 2021:
-        df = get_apc_parquet_data(spark, year)
-    else:
-        df = get_apc_csv_data(spark, year)
+    fyear = year * 100 + ((year + 1) % 100)
+    fn = get_apc_parquet_data if year >= 2021 else get_apc_csv_data
     
+    df = fn(spark, year)
+
+    # create the table if it does not exist
+    (
+        DeltaTable.createIfNotExists(spark)
+        .tableName(TABLE)
+        .addColumns(df.schema)
+        .execute()
+    )
+
+    # delete rows of data if they already existed
+    spark.sql(f"DELETE FROM {TABLE} WHERE fyear = {fyear}")
+
     # save data
     (
         df
@@ -21,7 +34,7 @@ def load_apc_data(spark: SparkContext, year: int) -> None:
         .write
         .option("mergeSchema", "true")
         .mode("append")
-        .saveAsTable("hes.bronze.apc")
+        .saveAsTable(TABLE)
     )
 
 
